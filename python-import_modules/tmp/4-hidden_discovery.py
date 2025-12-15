a32ca6599b1e0753fbc5a528e623305009852c06
Omer2121212121 <mirzeyevomer1@gmail.com>
2025-12-15 13:52:52 +0000

    Task3

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
-
     names = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
-            print(name)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
